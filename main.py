0421ac724f0352ffae846562a8b6c6033574f9c2
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-10-21 00:25:57 +0700

    edit endpoints

--- main.py
+++ main.py
@@ -19,11 +19,12 @@ async def root():
 
 @app.post("/add-user/", response_model=schemas.User)
 async def create_user(user: schemas.User, db: AsyncSession=Depends(get_session)):
-    return crud.create_user(db=db, user=user)
+    return await crud.create_user(db=db, user=user)
 
-@app.get("/users/", response_model=List[schemas.User])
+@app.get("/users/",)
 async def get_user(db: AsyncSession=Depends(get_session)):
     user = await crud.get_user_by_id(db)
+    print(user)
     if user is None:
         raise HTTPException(status_code=404, detail="Not found")
-    return user
+    return {'users': user}
