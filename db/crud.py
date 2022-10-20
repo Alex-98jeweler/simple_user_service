d0d466d70156a60b216abf487fda50bdc09ec66f
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-10-20 23:18:50 +0700

    chnage main and crud operations

--- db/crud.py
+++ db/crud.py
@@ -1,11 +1,14 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 import db.models as models
 import db.schemas as schemas
 
 
-def get_user_by_id(db: Session, id: int):
-    return db.query(models.User).get(id)
+async def get_user_by_id(db: AsyncSession):
+    result = await db.execute(select(models.User).order_by(models.User.id))
+    return result.scalar().all()
 
 
 def create_user(db: Session, user:schemas.User):
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+from typing import List
+from webbrowser import get
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 import db.crud as crud, db.models as models, db.schemas as schemas
-from db.database import SessionLocal, engine
+from db.database import engine, AsyncSession, get_session
 
 
 app = FastAPI()
 
 
 #Dependency
-def get_db():
-    db = SessionLocal()
-    try: yield db
-    finally: db.close()
 
 
 @app.get("/")
@@ -20,12 +18,12 @@ async def root():
     return "Piska siska"
 
 @app.post("/add-user/", response_model=schemas.User)
-async def create_user(user: schemas.User, db: Session=Depends(get_db)):
+async def create_user(user: schemas.User, db: AsyncSession=Depends(get_session)):
     return crud.create_user(db=db, user=user)
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-async def get_user(user_id, db: Session=Depends(get_db)):
-    user = crud.get_user_by_id(db, user_id)
+@app.get("/users/", response_model=List[schemas.User])
+async def get_user(db: AsyncSession=Depends(get_session)):
+    user = await crud.get_user_by_id(db)
     if user is None:
         raise HTTPException(status_code=404, detail="Not found")
     return user
