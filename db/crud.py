7a7d157f13df6a70352b67ddc83043e090b82101
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-10-21 00:25:35 +0700

    edit crud operations

--- db/crud.py
+++ db/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+import sqlalchemy.engine.row as r
 
 import db.models as models
 import db.schemas as schemas
@@ -8,12 +9,22 @@ import db.schemas as schemas
 
 async def get_user_by_id(db: AsyncSession):
     result = await db.execute(select(models.User).order_by(models.User.id))
-    return result.scalar().all()
+    data = result.fetchall()
+    results = []
+    for i in data:
+        user = schemas.User.parse_obj(i["User"].__dict__)
+        results.append(user)
+        print(i["User"].__dict__)
+    return results
 
 
-def create_user(db: Session, user:schemas.User):
-    user = models.User(firstname=user.firstname, lastname=user.lastname, email=user.email, age=user.age)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+async def create_user(db: AsyncSession, user:schemas.User):
+    user_db = models.User()
+    user_db.firstname = user.firstname
+    user_db.lastname = user.lastname
+    user_db.email = user.email
+    user_db.age = user.age
+    db.add(user_db)
+    await db.commit()
+    await db.refresh(user_db)
+    return user.__dict__
